8fe4d8eb54a2a774c91ce16cc6efd9ad71ccc153
Dominik <dominik@pfaffenbauer.at>
2023-02-16 08:20:20 +0100

    Update plugins/scaffolder-backend-module-gitlab/src/actions/createProjectDeployToken.test.ts
    Co-authored-by: Andreas Berger <Andy2003@users.noreply.github.com>
    Signed-off-by: Dominik <dominik@pfaffenbauer.at>

--- plugins/scaffolder-backend-module-gitlab/src/actions/createProjectDeployToken.test.ts
+++ plugins/scaffolder-backend-module-gitlab/src/actions/createProjectDeployToken.test.ts
@@ -95,5 +95,9 @@ describe('gitlab:create-deploy-token', () => {
       ['read_repository'],
       { username: 'tokenuser' },
     );
+
+    expect(mockContext.output).toHaveBeenCalledWith('deploy_token', 'TOKEN');
+    expect(mockContext.output).toHaveBeenCalledWith('user', 'User');
+    
   });
 });
